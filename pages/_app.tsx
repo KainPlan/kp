685453ee07c225054933d4e22924f4a0f4319701
Matthias Monschein <monschein.matthias@gmail.com>
2020-09-01 13:23:13 +0200

    :dizzy: nice page transitions - nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,38 @@
 import { appWithTranslation } from '../i18n';
 import '../styles/global.scss';
+import NProgress from 'nprogress';
+import '../styles/nprogress.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-const KPApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+function KPApp({ Component, pageProps }): React.ReactElement {
+  const router = useRouter();
+
+  NProgress.configure({ showSpinner: false, });
+
+  useEffect(() => {
+    const changeStart = () => {
+      NProgress.start();
+    };
+    const changeComplete = () => {
+      NProgress.done();
+    };
+    const changeError = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', changeStart);
+    router.events.on('routeChangeComplete', changeComplete);
+    router.events.on('routeChangeError', changeError);
+
+    return () => {
+      router.events.off('routeChangeStart', changeStart);
+      router.events.off('routeChangeComplete', changeComplete);
+      router.events.off('routeChangeError', changeError);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
 export default appWithTranslation(KPApp);
